343d405a63c15e1ed7baf23ad4f1f5a1385bfe7b
John Argyroulis <johnargyroulis@gmail.com>
2021-04-06 15:24:14 +0200

    extra comment for clarification

--- hazelcast/src/main/java/com/hazelcast/cp/internal/NodeEngineRaftIntegration.java
+++ hazelcast/src/main/java/com/hazelcast/cp/internal/NodeEngineRaftIntegration.java
@@ -257,9 +257,11 @@ final class NodeEngineRaftIntegration implements RaftIntegration{
 
         operation.setTargetEndpoint(target).setPartitionId(partitionId);
 
+        // Use RDMA if possible
         if(rdmaService.getLatestState().equals(RdmaServiceState.CONNECTIONS_READY)){
             return rdmaService.send(operation, targetMember.getAddress());
         }
+        // Otherwise use TCP
         return operationService.send(operation, targetMember.getAddress());
     }
 
